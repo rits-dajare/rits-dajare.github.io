a65fdb237cd31091ad36486128be7badf331e315
ygkn <2000ygkn0713@gmail.com>
2020-03-16 02:11:12 +0900

    Implement Smooth Scroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,7 @@
 import 'ress/dist/ress.min.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import smoothScroll from 'smooth-scroll';
 
 config.autoAddCss = false;
+smoothScroll('a[href*="#"]', { speed: 600 });
