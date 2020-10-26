6a534de9c864ae8336ae8ef320b075152e9f5bc2
ygkn <2000ygkn0713@gmail.com>
2020-10-27 06:05:25 +0900

    add apple-touch-icon setting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,7 @@ class MyDocument extends Document {
           <link rel="shortcut icon" type="image/svg+xml" href="/favicon.svg" />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#B61F38" />
+          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         </Head>
         <body className="h-full font-sans bg-off-white text-off-back">
           <Main />
