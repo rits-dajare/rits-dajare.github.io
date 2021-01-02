f9b9164d9625a2eee89b4253cc34957566ded6c7
ygkn <2000ygkn0713@gmail.com>
2021-01-02 12:49:28 +0900

    year++

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 const Footer: FC = () => (
   <footer className="text-center text-off-black dark:text-off-white my-5">
-    © 2020 立命館ダジャレサークル
+    © 2021 立命館ダジャレサークル
   </footer>
 );
 
