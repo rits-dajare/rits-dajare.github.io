e47fba97d7d5272b6feb31a72fae1b4a71fe1653
ygkn <2000ygkn0713@gmail.com>
2021-01-29 00:16:39 +0900

    fix for SEO

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,4 +1,4 @@
-import { NextSeo } from 'next-seo';
+import { LogoJsonLd, NextSeo, SocialProfileJsonLd } from 'next-seo';
 import { VFC } from 'react';
 
 const SEO: VFC<{ title: string; path: string; description: string }> = ({
@@ -10,31 +10,47 @@ const SEO: VFC<{ title: string; path: string; description: string }> = ({
   const fullTitle = `${title && `${title} | `}立命館ダジャレサークル`;
 
   return (
-    <NextSeo
-      title={fullTitle}
-      canonical={url}
-      description={description}
-      openGraph={{
-        type: 'website',
-        locale: 'ja_JP',
-        url,
-        site_name: fullTitle,
-        description,
-        images: [
-          {
-            url: 'https://rits-dajare.github.io/ogp.png',
-            width: 4096,
-            height: 4096,
-            alt: '立命館ダジャレサークル',
-          },
-        ],
-      }}
-      twitter={{
-        handle: '@rits_dajare',
-        site: '@rits_dajare',
-        cardType: 'summary',
-      }}
-    />
+    <>
+      <NextSeo
+        title={fullTitle}
+        canonical={url}
+        description={description}
+        openGraph={{
+          type: 'website',
+          locale: 'ja_JP',
+          url,
+          site_name: fullTitle,
+          description,
+          images: [
+            {
+              url: 'https://rits-dajare.github.io/ogp.png',
+              width: 4096,
+              height: 4096,
+              alt: '立命館ダジャレサークル',
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@rits_dajare',
+          site: '@rits_dajare',
+          cardType: 'summary',
+        }}
+      />
+
+      <LogoJsonLd
+        logo="https://rits-dajare.github.io/ogp.png"
+        url="http://rits-dajare.github.io/"
+      />
+      <SocialProfileJsonLd
+        type="Organization"
+        name="立命館ダジャレサークル"
+        url="http://rits-dajare.github.io/"
+        sameAs={[
+          'https://twitter.com/rits_dajare',
+          'https://github.com/rits-dajare',
+        ]}
+      />
+    </>
   );
 };
 
