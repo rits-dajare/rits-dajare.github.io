ec134b862862b342533b54bc98bd450d03db3ed3
ygkn <2000ygkn0713@gmail.com>
2021-05-15 18:24:46 +0900

    add lib of API to DaaS

---
+++ src/lib/api/dajare.ts
@@ -0,0 +1,55 @@
+import { z } from 'zod';
+
+const baseUrl = 'https://api.abelab.dev/daas';
+
+export type APIParams = {
+  '/judge': {
+    dajare: string;
+  };
+  '/eval': {
+    dajare: string;
+  };
+};
+
+const APIResponseSchemes = {
+  '/judge': z.object({
+    status: z.union([z.literal('OK'), z.literal('error')]),
+    message: z.string(),
+    is_dajare: z.boolean(),
+  }),
+  '/eval': z.object({
+    status: z.union([z.literal('OK'), z.literal('error')]),
+    message: z.string(),
+    score: z.number(),
+  }),
+};
+
+export type APIResponses = {
+  [K in keyof typeof APIResponseSchemes]: z.infer<typeof APIResponseSchemes[K]>;
+};
+
+export const fetchDajareAPI = async <
+  Path extends keyof APIParams | keyof APIResponses
+>(
+  path: Path,
+  params: APIParams[Path]
+): Promise<APIResponses[Path]> => {
+  const paramString = new URLSearchParams(params).toString();
+
+  console.log(`${baseUrl}${path}?${paramString}`);
+
+  const result = await fetch(`${baseUrl}${path}?${paramString}`);
+
+  const resultJSON = await result.json();
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const response: APIResponses[Path] =
+    APIResponseSchemes[path].parse(resultJSON);
+
+  if (response.status !== 'OK') {
+    throw new Error(resultJSON.message);
+  }
+
+  return response;
+};
